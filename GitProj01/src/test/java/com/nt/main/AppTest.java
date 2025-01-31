a99563254daa107a92cbfa7742c89d4b85597ad8
parveen9949 <achukatlaparveen@gmail.com>
2025-01-31 12:04:35 +0530

    TL commit the gitproj01

--- GitProj01/src/test/java/com/nt/main/AppTest.java
+++ GitProj01/src/test/java/com/nt/main/AppTest.java
@@ -39,6 +39,14 @@ public class AppTest
         int actual=app.sum(-100,-200);
         assertEquals(expected,actual);
     }
+    public void testSumWithZeros()
+    {
+        App app=new App();
+        int expected=0;
+        int actual=app.sum(0,0);
+        assertEquals(expected,actual);
+    }
+
 
 
 }
