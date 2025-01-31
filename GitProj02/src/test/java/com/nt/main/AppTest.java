97ae21baa2953fe2c08c71a18fb581d5a9388fcb
parveen9949 <achukatlaparveen@gmail.com>
2025-01-31 22:05:35 +0530

    DEV1 some text

--- GitProj02/src/test/java/com/nt/main/AppTest.java
+++ GitProj02/src/test/java/com/nt/main/AppTest.java
@@ -1,19 +1,40 @@
 package com.nt.main;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
-    }
+        @Test
+    public void sumWithPositives()
+        {
+        	App app=new App();
+        	int expected=300;
+        	int actual=app.sum(100, 200);
+            assertEquals(expected,actual);
+            }
+        public void sumWithNegatives()
+        {
+        	App app=new App();
+        	int expected=100;
+        	int actual=app.sum(-100, 200);
+            assertEquals(expected,actual);
+            }
+        public void sumWithMixed()
+        {
+        	App app=new App();
+        	int expected=-300;
+        	int actual=app.sum(-100,-200);
+            assertEquals(expected,actual);
+            }
+        public void sumWithZeros()
+        {
+        	App app=new App();
+        	int expected=0;
+        	int actual=app.sum(0, 0);
+            assertEquals(expected,actual);
+            }
 }
