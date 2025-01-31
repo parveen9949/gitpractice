20c0bdf8b5cc2d6110177e0560f7e3b220ca0ab6
parveen9949 <achukatlaparveen@gmail.com>
2025-01-31 11:33:54 +0530

    Dev1 commit the GitProj01

--- GitProj01/src/main/java/com/nt/main/App.java
+++ GitProj01/src/main/java/com/nt/main/App.java
@@ -6,8 +6,14 @@ package com.nt.main;
  */
 public class App 
 {
+	public int sum(int x,int y) 
+	{
+		return x+y;
+		
+	}
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App app=new App();
+        System.out.println("sum is::"+app.sum(10, 20));
     }
 }
--- GitProj01/src/test/java/com/nt/main/AppTest.java
+++ GitProj01/src/test/java/com/nt/main/AppTest.java
@@ -4,35 +4,41 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
+   
     public AppTest( String testName )
     {
         super( testName );
     }
-
-    /**
-     * @return the suite of tests being tested
-     */
     public static Test suite()
     {
         return new TestSuite( AppTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
+    public void testSumWithPositives()
     {
-        assertTrue( true );
+        App app=new App();
+        int expected=300;
+        int actual=app.sum(100,200);
+        assertEquals(expected,actual);
     }
+    public void testSumWithNegatives()
+    {
+        App app=new App();
+        int expected=-100;
+        int actual=app.sum(100,-200);
+        assertEquals(expected,actual);
+    }
+    public void testSumWithMixedValues()
+    {
+        App app=new App();
+        int expected=-300;
+        int actual=app.sum(-100,-200);
+        assertEquals(expected,actual);
+    }
+
+
 }
